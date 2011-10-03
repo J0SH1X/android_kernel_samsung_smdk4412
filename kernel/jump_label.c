312d7e6ec68ff2fc0df45d8219ed6817871ded9b
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2011-10-03 11:01:46 -0700

    jump_label: add arch_jump_label_transform_static() to optimise non-live code updates
    When updating a newly loaded module, the code is definitely not yet
    executing on any processor, so it can be updated with no need for any
    heavyweight synchronization.
    
    This patch adds arch_jump_label_static() which is implemented as
    arch_jump_label_transform() by default, but architectures can override
    it if it avoids, say, a call to stop_machine().
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Acked-by: Jason Baron <jbaron@redhat.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -136,6 +136,18 @@ static int __jump_label_text_reserved(struct jump_entry *iter_start,
 	return 0;
 }
 
+/* 
+ * Update code which is definitely not currently executing.
+ * Architectures which need heavyweight synchronization to modify
+ * running code can override this to make the non-live update case
+ * cheaper.
+ */
+void __weak arch_jump_label_transform_static(struct jump_entry *entry,
+					    enum jump_label_type type)
+{
+	arch_jump_label_transform(entry, type);	
+}
+
 static void __jump_label_update(struct jump_label_key *key,
 				struct jump_entry *entry,
 				struct jump_entry *stop, int enable)
@@ -164,8 +176,12 @@ void __init jump_label_init(void)
 	jump_label_sort_entries(iter_start, iter_stop);
 
 	for (iter = iter_start; iter < iter_stop; iter++) {
-		arch_jump_label_text_poke_early(iter->code);
-		if (iter->key == (jump_label_t)(unsigned long)key)
+		struct jump_label_key *iterk;
+
+		iterk = (struct jump_label_key *)(unsigned long)iter->key;
+		arch_jump_label_transform_static(iter, jump_label_enabled(iterk) ?
+						 JUMP_LABEL_ENABLE : JUMP_LABEL_DISABLE);
+		if (iterk == key)
 			continue;
 
 		key = (struct jump_label_key *)(unsigned long)iter->key;
@@ -234,7 +250,7 @@ void jump_label_apply_nops(struct module *mod)
 		return;
 
 	for (iter = iter_start; iter < iter_stop; iter++)
-		arch_jump_label_text_poke_early(iter->code);
+		arch_jump_label_transform_static(iter, JUMP_LABEL_DISABLE);
 }
 
 static int jump_label_add_module(struct module *mod)
