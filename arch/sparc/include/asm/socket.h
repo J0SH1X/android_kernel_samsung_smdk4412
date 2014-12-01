274f60feb022e3ec2ce97144293df6e3663a89f5
Alexei Starovoitov <ast@plumgrid.com>
2014-12-01 15:06:35 -0800

    net: sock: allow eBPF programs to be attached to sockets
    introduce new setsockopt() command:
    
    setsockopt(sock, SOL_SOCKET, SO_ATTACH_BPF, &prog_fd, sizeof(prog_fd))
    
    where prog_fd was received from syscall bpf(BPF_PROG_LOAD, attr, ...)
    and attr->prog_type == BPF_PROG_TYPE_SOCKET_FILTER
    
    setsockopt() calls bpf_prog_get() which increments refcnt of the program,
    so it doesn't get unloaded while socket is using the program.
    
    The same eBPF program can be attached to multiple sockets.
    
    User task exit automatically closes socket which calls sk_filter_uncharge()
    which decrements refcnt of eBPF program
    
    Signed-off-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: Ic246d5954b45e84c4f94f18629cea71d24df7486

--- arch/sparc/include/asm/socket.h
+++ arch/sparc/include/asm/socket.h
@@ -69,6 +69,9 @@
 #define SO_LOCK_FILTER          0x0028
 #define SO_MAX_PACING_RATE	0x002B
 
+#define SO_ATTACH_BPF		0x0034
+#define SO_DETACH_BPF		SO_DETACH_FILTER
+
 /* Security levels - as per NRL IPv6 - don't actually do anything */
 #define SO_SECURITY_AUTHENTICATION		0x5001
 #define SO_SECURITY_ENCRYPTION_TRANSPORT	0x5002
